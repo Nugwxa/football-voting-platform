343ac241a272d664108af57138b0c46069d53ca4
OJ Abba <phavorabba@gmail.com>
2024-04-01 05:39:39 +0100

    Add auth to user model

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,10 +1,15 @@
-type VoteCandidate = {
-id: String
-name: String
-imgURL: String // Imgur
+type UserAuth = {
+  passwordHash: String
+  passwordSalt: String
 }
 
 type ActionResponse = {
-    type: "error" | "success"
-    message: String
+  type: 'error' | 'success'
+  message: String
+}
+
+type VoteCandidate = {
+  id: String
+  name: String
+  imgURL: String // Imgur
 }
