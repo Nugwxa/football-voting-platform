a1c8fdc33fe1e16b7259ba542585cb50e96167ba
OJ Abba <phavorabba@gmail.com>
2024-06-30 17:18:40 +0100

    Add password reset page (Email)

---
+++ src/app/auth/reset/page.tsx
@@ -0,0 +1,22 @@
+import { ResetPasswordEmailForm } from './client'
+import authStyle from '../auth.layout.module.css'
+import Link from 'next/link'
+import TextWithDivider from '@/components/TextWithDivider/TextWithDivider'
+
+export default async function Page() {
+  return (
+    <div className={authStyle.contentContainer}>
+      <h1 className={authStyle.heading}>Reset Password</h1>
+
+      <ResetPasswordEmailForm />
+
+      <TextWithDivider>or</TextWithDivider>
+
+      <p className={authStyle.redirectText}>
+        <Link className={authStyle.singleAnchor} href={'/auth/reset'}>
+          Back To Login
+        </Link>
+      </p>
+    </div>
+  )
+}
