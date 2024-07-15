4d5cbed69442ff0498380afef163814a3b629a52
OJ Abba <phavorabba@gmail.com>
2024-07-15 22:58:25 +0100

    Begin dashboard

---
+++ src/app/(main)/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import readSession from '@/lib/session'
+import styles from './page.module.css'
+import { notFound } from 'next/navigation'
+
+export default async function Page() {
+  const session = await readSession()
+  if (!session || !session.user.isAdmin) return notFound()
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <div className={styles.body}>
+        <section>
+          <div className={styles.dashboardItemsWrapper}>Dashboard content</div>
+        </section>
+      </div>
+    </>
+  )
+}
