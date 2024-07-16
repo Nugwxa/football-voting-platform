5775deb97d9c45d34df861f35587f1f90d8f6ec2
OJ Abba <phavorabba@gmail.com>
2024-07-16 23:38:40 +0100

    Add  dashboard stats to dashboard

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,16 +1,43 @@
+import { DashboardStatContainer } from './client'
+import { HandHelpingIcon, UsersIcon, VoteIcon } from 'lucide-react'
 import { notFound } from 'next/navigation'
+import prisma from '@lib/prisma'
 import readSession from '@/lib/session'
 import styles from './page.module.css'
 
 export default async function Page() {
   const session = await readSession()
   if (!session || !session.user.isAdmin) return notFound()
+
+  const userCount = await prisma.user.count()
+  const pollCount = await prisma.vote.count()
+  const voteCount = await prisma.voteRegigtration.count()
+  const iconSize = 20
   return (
     <>
       <h1>Dashboard</h1>
       <div className={styles.body}>
-        <section>
-          <div className={styles.dashboardItemsWrapper}>Dashboard content</div>
+        <section className={styles.section}>
+          <h2>Stats</h2>
+          <div className={styles.dashboardItemsWrapper}>
+            <DashboardStatContainer
+              count={userCount}
+              text="Users"
+              icon={<UsersIcon size={iconSize} />}
+            />
+
+            <DashboardStatContainer
+              count={pollCount}
+              text="Polls"
+              icon={<VoteIcon size={iconSize} />}
+            />
+
+            <DashboardStatContainer
+              count={voteCount}
+              text="Registered Votes"
+              icon={<HandHelpingIcon size={iconSize} />}
+            />
+          </div>
         </section>
       </div>
     </>
