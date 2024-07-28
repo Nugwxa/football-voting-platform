7db8195be0b8e9e27af7aecb8d597d474fdead2f
OJ Abba <phavorabba@gmail.com>
2024-07-28 21:54:28 +0100

    Remove TODO

--- src/components/TextWithDivider/TextWithDivider.tsx
+++ src/components/TextWithDivider/TextWithDivider.tsx
@@ -1,7 +1,6 @@
 import style from './style.module.css'
 
 export default function TextWithDivider({ children }: { children: any }) {
-  // TODO: Make Color A Prop
   return (
     <p className={style.dividerText}>
       <span>{children}</span>
