506f9446b6ab30a39d5594e84ee6fcfc2c6f8d52
OJ Abba <phavorabba@gmail.com>
2024-07-29 01:12:56 +0100

    Refactor HeaderMenu

--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -6,18 +6,21 @@ import {
   NavItem,
 } from './client'
 import { LayoutDashboardIcon, VoteIcon } from 'lucide-react'
+import { readSession } from '@/lib/session'
 import classNames from 'classnames'
-import Image from 'next/image'
 import Link from 'next/link'
-import readSession from '@/lib/session'
 import styles from './HeaderMenu.module.css'
-type AnchorLink = {
+
+export type AnchorLink = {
   href: string
   label: string
   viewCondition: boolean
   icon: JSX.Element
 }
 
+/**
+ * Renders the header menu
+ */
 export default async function HeaderMenu() {
   const session = await readSession()
   const name = session?.user.name ?? 'null'
@@ -42,13 +45,9 @@ export default async function HeaderMenu() {
       <div className={styles.contentContainer}>
         <div className={styles.headerLeft}>
           <div className={styles.logoArea}>
-            <Image
-              className={styles.logo}
-              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAARCAMAAAC/+5/SAAAAY1BMVEX///8AAAC9vb2wsLA6OjpSUlLGxsbJyckyMjLf39/X19fo6OisrKy5ubknJycaGhqKioqSkpL5+flZWVl9fX0sLCxHR0fx8fEUFBTPz89CQkJtbW1eXl5oaGhycnJMTEyenp6UHuiQAAABM0lEQVQokZWT6ZaDIAyFExcqijtuba2+/1OWQHDQzsw5vb8uiV8wMQKQhL5X2aOEL5XXiFg1CWLkQ10Zs+gQOSugULGSB1ciaQEonthyKTzk80YrCAyKu4deZBfE2MYwAH+KPOAWgjUXNp1i+8LOGGkCSeoEPWLdk9uaEygsF1nXQIc7g7EvbHzqfQhq5AxxAM/hCmZ/gCOBE0BkOVDIIEvBzi7Nz6ApSK8XuYFOPTUZgDGXJonrjdnBKTcmAquMVNFSqI0cpS49SubkgGr96DFoL19OU22YazCd4HOqAXhb7Auz5tFywtZaj+9YJVZ3kM4NJtYRONjjZjdnp6Foe+l/m6ODRAU0mHHHzOxukfpdlb+BVHnxh5key2cTbXX4d4CIWDCVzkw23nGCOxU6mb/8H9/mCQ0lzHEXEAAAAABJRU5ErkJggg=="
-              alt="Logo"
-              fill
-              priority
-            />
+            <Link className={styles.textLogo} href={'/'}>
+              Tally Vote
+            </Link>
           </div>
           {session && (
             <nav className={styles.headerNav}>
