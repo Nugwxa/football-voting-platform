00078d9eb1e53c5ef521aa314dcf7996de597ab2
OJ Abba <phavorabba@gmail.com>
2024-08-19 06:08:14 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import HeaderMenu from '@/components/HeaderMenu'
-import styles from './page.module.css'
 
 export default async function Home() {
   return (
-    <main className={styles.mainn}>
+    <main>
       <HeaderMenu />
     </main>
   )
