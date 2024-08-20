cc9ca0c20a8c6c6130c1cd70640dffdee815570d
OJ Abba <phavorabba@gmail.com>
2024-08-20 01:27:03 +0100

    Update auth layout styles

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import style from './auth.layout.module.css'
 
 export default function AuthLayout({
@@ -6,8 +7,29 @@ export default function AuthLayout({
   children: React.ReactNode
 }>) {
   return (
-    <main className={style.main}>
-      <div className={style.container}>{children}</div>
-    </main>
+    <>
+      <nav className={style.authNav}>
+        <div className={style.authNavLogoWrapper}>
+          <Image
+            priority
+            sizes="(max-width: 768px) 58px"
+            src={'/img/team9_logo.png'}
+            alt="Team 9 Logo"
+            fill
+          />
+        </div>
+      </nav>
+
+      {/* Background Design Section*/}
+      <section className={style.authBackgroundSection}>
+        {/* Content set from class/css */}
+      </section>
+
+      {/* Main  Section*/}
+      <main className={style.authMain}>
+        {/* Auth Body Container */}
+        <div className={style.authBodyContainer}>{children}</div>
+      </main>
+    </>
   )
 }
