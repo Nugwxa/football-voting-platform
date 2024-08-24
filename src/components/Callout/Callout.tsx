a99c78b4a1309413e3140dad171e2c2704705283
OJ Abba <phavorabba@gmail.com>
2024-08-24 18:40:21 +0100

    Update Callout icon to JSX

--- src/components/Callout/Callout.tsx
+++ src/components/Callout/Callout.tsx
@@ -1,12 +1,11 @@
 'use client'
-import { LucideIcon } from 'lucide-react'
 import classNames from 'classnames'
 import style from './callout.module.css'
 
 interface CalloutProps extends React.ComponentPropsWithoutRef<'div'> {
   children: string
   className?: string
-  icon: LucideIcon
+  icon: JSX.Element
   isWide?: boolean
   title?: string
 }
@@ -17,18 +16,11 @@ interface CalloutProps extends React.ComponentPropsWithoutRef<'div'> {
  * @param {string} className - Additional class name to apply to the callout.
  * @param {string} title - Optional title text to display at the top of the callout.
  * @param {string} children - Text content to display within the callout.
- * @param {LucideIcon} icon - Icon to display in the callout. Must be a LucideIcon component.
+ * @param {JSX.Element} icon - Icon to display in the callout.
  * @param {boolean} isWide - Optional flag to apply wide styling to the callout.
  */
 export default function Callout(props: Readonly<CalloutProps>) {
-  const {
-    children,
-    className,
-    icon: Icon,
-    isWide = false,
-    title,
-    ...rest
-  } = props
+  const { children, className, icon, isWide = false, title, ...rest } = props
   return (
     <div
       className={classNames(className, style.calloutWrapper, {
@@ -37,9 +29,7 @@ export default function Callout(props: Readonly<CalloutProps>) {
       {...rest}
     >
       <div className={style.calloutContainer}>
-        <div className={style.iconWrapper}>
-          <Icon size={16} />
-        </div>
+        <div className={style.iconWrapper}>{icon}</div>
         <div className={style.calloutTextSection}>
           <div className={style.calloutTitle}>{title}</div>
           <div className={style.calloutText}>{children}</div>
