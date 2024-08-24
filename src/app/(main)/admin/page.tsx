921fc99afa6d4b88b0226684e6953ee8391ce04b
OJ Abba <phavorabba@gmail.com>
2024-08-24 19:17:08 +0100

    Complete users page

--- src/app/(main)/admin/page.tsx
+++ src/app/(main)/admin/page.tsx
@@ -1,34 +1,61 @@
 import { adminNavLinks } from './data'
+import { AlertTriangleIcon } from 'lucide-react'
+import { countUsers } from '@/lib/user'
+import { readSession } from '@/lib/session'
 import adminStyles from './adminStyles.module.css'
+import Callout from '@/components/Callout'
 import ContentWrapper from '@/components/ContentWrapper'
 import PageHeader from '@/components/PageHeader'
+import Pagination from '@/components/Pagination/Pagination'
 import SearchInput from '@/components/SearchInput'
 import UserTable from './_components/UserTable'
 
 type SearchParam = {
   query?: string
   page?: string
+  sort?: string
 }
 interface AdminUsersPageProps {
   searchParams: SearchParam
 }
 export default async function Page(props: Readonly<AdminUsersPageProps>) {
-  const { searchParams } = props
+  const session = await readSession()
 
+  // Restrict access if the user is not authenticated
+  if (!session || !session.user.isAdmin)
+    return (
+      <ContentWrapper>
+        <Callout title="Restricted Page!" icon={<AlertTriangleIcon />} isWide>
+          You don't have the permission to view this page
+        </Callout>
+      </ContentWrapper>
+    )
+  const { searchParams } = props
   const page = searchParams.page ?? 1
   const query = searchParams.query ?? ''
+  const sort = searchParams.sort ?? 'joined-newest'
+
+  // Count the total number of users based on the search query
+  const userCount = await countUsers(query)
+  const perPage = 10
   return (
     <>
+      {/* Page header with navigation links */}
       <PageHeader title="Users" links={adminNavLinks} />
       <ContentWrapper>
         <section className={adminStyles.headingActionsSection}>
+          {/* Search input for filtering users */}
           <SearchInput
             className={adminStyles.searchInput}
             placeholder="Search users ..."
           />
         </section>
 
-        <UserTable page={Number(page)} query={query} />
+        {/* User table displaying the list of users */}
+        <UserTable page={Number(page)} query={query} sort={sort} />
+
+        {/* Pagination component to navigate through pages */}
+        <Pagination totalPages={Math.ceil(userCount / perPage)} />
       </ContentWrapper>
     </>
   )
