6c81e7195282a33e078f36601180210c06d178f5
OJ Abba <phavorabba@gmail.com>
2024-08-25 19:00:46 +0100

    Create isValidPosition function

--- src/app/(main)/admin/data.ts
+++ src/app/(main)/admin/data.ts
@@ -1,4 +1,5 @@
 import { NavLinkType } from '@/components/NavLinks/NavItem/NavItem'
+import { $Enums } from '@prisma/client'
 
 export const adminNavLinks: NavLinkType[] = [
   {
@@ -19,3 +20,23 @@ export const adminNavLinks: NavLinkType[] = [
     viewCondition: true,
   },
 ]
+
+/**
+ * Checks if a given position string is a valid player position.
+ *
+ * @param {string} position - The position string to validate.
+ * @returns {position is $Enums.PlayerPositions} - Returns `true` if the
+ * position is valid, otherwise `false`.
+ */
+export function isValidPosition(
+  position: string
+): position is $Enums.PlayerPositions {
+  const positions: Set<string> = new Set([
+    'goalkeeper',
+    'defender',
+    'midfielder',
+    'forward',
+  ])
+
+  return positions.has(position)
+}
