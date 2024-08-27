5eae0c41ab4bcd7954e0575f238d767e7e36a6ac
OJ Abba <phavorabba@gmail.com>
2024-08-27 10:01:30 +0100

    Add custom styling to ReactSelect

--- src/components/ReactSelect/ReactSelect.tsx
+++ src/components/ReactSelect/ReactSelect.tsx
@@ -1,14 +1,61 @@
 'use client'
+import classNames from 'classnames'
 import React from 'react'
 import Select, { Props as SelectProps, GroupBase } from 'react-select'
 interface OptionType {
   value: string
   label: string
 }
+import styles from './ReactSelect.module.css'
 
 interface ReactSelectProps
-  extends SelectProps<OptionType, false, GroupBase<OptionType>> {}
+  extends SelectProps<OptionType, boolean, GroupBase<OptionType>> {}
 
+/**
+ * React select wrapper component
+ */
 export default function ReactSelect(props: Readonly<ReactSelectProps>) {
-  return <Select {...props} />
+  const { className, ...rest } = props
+  return (
+    <Select
+      className={classNames(className, styles.select)}
+      // Override styling
+      classNames={{
+        input: (state) => {
+          return styles.input
+        },
+        container: (state) => {
+          return styles.container
+        },
+        control: (state) => {
+          return styles.control
+        },
+        option: (state) => {
+          return classNames(styles.option, {
+            [styles.optionSelected]: state.isSelected,
+            [styles.optionFocused]: state.isFocused,
+          })
+        },
+        multiValue: (state) => {
+          return styles.multiValue
+        },
+        multiValueLabel: (state) => {
+          return styles.multiValueGeneric
+        },
+        multiValueRemove: (state) => {
+          return styles.multiValueRemove
+        },
+        clearIndicator: (state) => {
+          return styles.clearIndicator
+        },
+        indicatorSeparator: (state) => {
+          return styles.indicatorSeparator
+        },
+        dropdownIndicator: (state) => {
+          return styles.dropdownIndicator
+        },
+      }}
+      {...rest}
+    />
+  )
 }
