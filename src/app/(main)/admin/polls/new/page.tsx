8e9af937e52195bfad4ee6deeaf201f5c9aee50a
OJ Abba <phavorabba@gmail.com>
2024-08-28 01:46:14 +0100

    Complete new polls page

---
+++ src/app/(main)/admin/polls/new/page.tsx
@@ -0,0 +1,21 @@
+import { adminNavLinks } from '@admin/data'
+import { Metadata } from 'next'
+import ContentWrapper from '@/components/ContentWrapper'
+import CreatePollForm from './_components/CreatePollForm'
+import PageHeader from '@/components/PageHeader'
+
+export const metadata: Metadata = {
+  title: 'New Poll (Admin) | Team 9 Voting',
+}
+export default async function Page() {
+  return (
+    <>
+      <PageHeader title="Polls" links={adminNavLinks} />
+      <ContentWrapper>
+        <section style={{ width: '100%' }}>
+          <CreatePollForm />
+        </section>
+      </ContentWrapper>
+    </>
+  )
+}
